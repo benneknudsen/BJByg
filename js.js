9cc6cba9d28cc8e6a5455389dba44d2a4e530d57
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-08-27 21:26:15 +0200

    Påbegyndt Single Page

--- js.js
+++ js.js
@@ -1,6 +1,34 @@
-
+"use strict"
 
 // Single page
+
+// hide all pages
+function hideAllPages() {
+  let pages = document.querySelectorAll(".page");
+  for (let page of pages) {
+    page.style.display = "none";
+  }
+}
+
+// show page or tab
+function showPage(pageId) {
+  hideAllPages();
+  document.querySelector(`#${pageId}`).style.display = "block";
+  location.href = `#${pageId}`;
+}
+
+
+// set default page
+function setDefaultPage() {
+  let page = "hjem";
+  if (location.hash) {
+    page = location.hash.slice(1);
+  }
+  showPage(page);
+}
+
+setDefaultPage();
+
 
 // Navbar | Burger Menu til mobil
 
