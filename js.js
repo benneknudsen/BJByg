9c4208a1d5017d3e52a0045d8e09997d46bfd3ab
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-08-27 20:51:25 +0200

    Navbar Færdiggjort

--- js.js
+++ js.js
@@ -1,31 +1,31 @@
-"use strict";
+
 
 // Single page
 
+// Navbar | Burger Menu til mobil
 
-// hide all pages
-function hideAllPages() {
-  let pages = document.querySelectorAll(".page");
-  for (let page of pages) {
-    page.style.display = "none";
-  }
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+
+  burger.addEventListener('click', () => {
+
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ''
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+    }
+
+    });
+
+    burger.classList.toggle('toggle');
+  });
+
 }
 
-// show page or tab
-function showPage(pageId) {
-  hideAllPages();
-  document.querySelector(`#${pageId}`).style.display = "block";
-  location.href = `#${pageId}`;
-}
-
-
-// set default page
-function setDefaultPage() {
-  let page = "home";
-  if (location.hash) {
-    page = location.hash.slice(1);
-  }
-  showPage(page);
-}
-
-setDefaultPage();
+navSlide();
