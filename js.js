a02fc31094b0974b397d7b6651b6f0d0b4578353
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-11-05 13:01:39 +0100

    Ny start

--- js.js
+++ js.js
@@ -1,67 +1,7 @@
 "use strict"
 
-// Single page
-
-// hide all pages
-function hideAllPages() {
-  let pages = document.querySelectorAll(".page");
-  for (let page of pages) {
-    page.style.display = "none";
-  }
-}
-
-// show page or tab
-function showPage(pageId) {
-  hideAllPages();
-  document.querySelector(`#${pageId}`).style.display = "block";
-  location.href = `#${pageId}`;
-}
-
-
-// set default page
-function setDefaultPage() {
-  let page = "hjem";
-  if (location.hash) {
-    page = location.hash.slice(1);
-  }
-  showPage(page);
-}
-
-setDefaultPage();
-
-
-// Navbar | Burger Menu til mobil
-
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-
-
-  burger.addEventListener('click', () => {
-
-    nav.classList.toggle('nav-active');
-
-    navLinks.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = ''
-    } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-    }
-
-    });
-
-    burger.classList.toggle('toggle');
-  });
-
-}
-
-navSlide();
-
 
 AOS.init({
 
   duration: 1000
 });
-
-$('.carousel').carousel()
